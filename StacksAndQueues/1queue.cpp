503b3e850802f4cd9e228249321efae0606196cd
riapandey05 <riapandey0805@gmail.com>
2025-05-21 23:01:32 +0530

    implementing queue using arrays

--- StacksAndQueues/1queue.cpp
+++ StacksAndQueues/1queue.cpp
@@ -8,19 +8,74 @@
 // isEmpty - to check if the queue is empty
 
 // now let us implement queue using array
+
 #include <iostream>
 using namespace std;
-class Myqueue{
-    private:
-        int arr[1000];
-        int front;
-        int end;
-    public:
-        Myqueue(){
-            front = -1;
-            end = -1;
-        }
-    void push(int x){
 
+class MyQueue {
+private:
+    int front, rear;
+    int size;
+    int* arr;
+
+public:
+    MyQueue(int capacity) {
+        size = capacity;
+        arr = new int[size];
+        front = 0;
+        rear = 0;
+    }
+
+    // Push operation
+    void push(int x) {
+        if (rear == size) {
+            cout << "Queue is full\n";
+            return;
+        }
+        arr[rear++] = x;
+    }
+
+    // Pop operation
+    int pop() {
+        if (front == rear) {
+            cout << "Queue is empty\n";
+            return -1;
+        }
+        return arr[front++];
+    }
+
+    // Display queue contents
+    void display() {
+        if (front == rear) {
+            cout << "Queue is empty\n";
+            return;
+        }
+        cout << "Queue: ";
+        for (int i = front; i < rear; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+
+    ~MyQueue() {
+        delete[] arr;
     }
 };
+
+int main() {
+    MyQueue q(10);  // Queue with capacity 10
+
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.display();
+
+    cout << "Popped: " << q.pop() << "\n";
+    q.display();
+
+    q.push(4);
+    q.push(5);
+    q.display();
+    cin.get();
+    return 0;
+}
