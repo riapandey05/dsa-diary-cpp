ff4e498e656f2ec94af9a9fbc894c863b1a6d991
riapandey05 <riapandey0805@gmail.com>
2025-08-31 09:25:24 +0530

    Time: 7 ms (73.45%) | Memory: 44.7 MB (74.23%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int l = 0; int r = 0;
+        int count = 0;
+        while(r<s.size() && l<g.size()){
+            if(g[l]<=s[r]){
+                l++;
+                r++;
+                count++;
+            }
+            else{
+                r++;
+            }
+        }
+        return count;
+    }
+};
