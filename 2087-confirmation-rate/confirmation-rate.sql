3914ce4d3fb3c32dc25fbfce3006d917db5a8624
riapandey05 <riapandey0805@gmail.com>
2025-12-11 21:39:32 +0530

    Time: 724 ms (14.98%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
+# Write your MySQL query statement below
 select Signups.user_id , round(avg(
     case
      when Confirmations.action = 'confirmed' then 1.00
@@ -6,5 +7,4 @@ select Signups.user_id , round(avg(
      end
 ),2) as confirmation_rate from Signups left join Confirmations on Signups.user_id = Confirmations.user_id group 
 by Signups.user_id;
-
 
